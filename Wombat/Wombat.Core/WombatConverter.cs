32ef8084d2cd171343e0ee705789c83a1cc88f3a
nikita.e <nikita.e@sbtech.com>
2016-04-20 18:30:33 +0300

    Initial Commit Base implementation and test Have fun =)

---
+++ Wombat/Wombat.Core/WombatConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wombat.Core
+{
+    public class WombatConverter
+    {
+        private readonly Dictionary<Type, Dictionary<Type, Func<object, object>>> converters;
+
+        public WombatConverter()
+        {
+            this.converters = new Dictionary<Type, Dictionary<Type, Func<object, object>>>();
+
+            this.Register<int, string>(integerValue => integerValue.ToString());
+        }
+
+        public bool IsSupported(Type sourceType, Type resultType)
+        {
+            return 
+                this.converters.ContainsKey(sourceType) 
+                && this.converters[sourceType].ContainsKey(resultType);
+        }
+
+        public TResult Convert<TSource, TResult>(TSource value)
+        {
+            var typeTResult = typeof(TResult);
+            var typeTSource = typeof(TSource);
+
+            if (!this.IsSupported(typeTSource, typeTResult))
+            {
+                throw new ArgumentException($"Pair {typeTSource.FullName} to {typeTResult.FullName} is not supported");
+            }
+
+            return (TResult)this.converters[typeTSource][typeTResult](value);
+        }
+
+        private void TryCreateSourceNode(Type typeTSource)
+        {
+            if (!this.converters.ContainsKey(typeTSource))
+            {
+                this.converters[typeTSource] = new Dictionary<Type, Func<object, object>>();
+            }
+        }
+
+        public void Register<TSource, TResult>(Func<TSource, TResult> converter)
+        {
+            var typeTResult = typeof(TResult);
+            var typeTSource = typeof(TSource);
+
+            if (this.IsSupported(typeof(TResult), typeof(TSource)))
+            {
+                throw new ArgumentException($"Pair {typeTSource.FullName} to {typeTResult.FullName} is alredy registered");
+            }
+
+            this.TryCreateSourceNode(typeTSource);
+
+            this.converters[typeTSource][typeTResult] = this.ConvertFunctionToBase(converter);
+        }
+
+        public void Ovveride<TSource, TResult>(Func<TSource, TResult> converter)
+        {
+            var typeTResult = typeof(TResult);
+            var typeTSource = typeof(TSource);
+
+            if (!this.IsSupported(typeTSource, typeTResult))
+            {
+                throw new ArgumentException($"Pair {typeTSource.FullName} to {typeTResult.FullName} is not supported");
+            }
+
+            this.TryCreateSourceNode(typeTSource);
+
+            this.converters[typeTResult][typeTResult] = this.ConvertFunctionToBase(converter);
+        }
+
+        private Func<object, object> ConvertFunctionToBase<TSource, TResult>(Func<TSource, TResult> converter)
+        {
+            return value => converter((TSource)value);
+        }
+    }
+}
---
+++ Wombat/Wombat.Test/ConverterTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Wombat.Test
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Wombat.Core;
+
+    [TestFixture]
+    public class ConverterTests
+    {
+        [Test]
+        public void Convert_ShouldConvertIntToString()
+        {
+            WombatConverter target = new WombatConverter();
+
+            var result = target.Convert<int, string>(5);
+
+            var expectedResult = "5";
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+        [Test]
+        public void Convert_ShouldConvertDoubleToInt_IfRegistered()
+        {
+            WombatConverter target = new WombatConverter();
+
+            target.Register<double, int>(doubleValue => (int)doubleValue);
+            var result = target.Convert<double, int>(5.1);
+
+            var expectedResult = 5;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+        [Test]
+        public void Convert_ShouldThrowsException_IfSuchConverterNotRegistered()
+        {
+            WombatConverter target = new WombatConverter();
+
+            target.Register<double, int>(doubleValue => (int)doubleValue);
+
+            Assert.Throws<ArgumentException>(() => target.Convert<double, string>(5.1));
+        }
+    }
+}
